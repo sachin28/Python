f54f1d430a31987197c76399c27c24c77e4299e8
sachin28 <shuklasachin.ec@gmail.com>
2016-10-09 15:21:09 -0700

    Updated the README

---
+++ Test Programs/stringreplacement.py
@@ -0,0 +1,7 @@
+str = " This is to inform Tom that Tony is going to Toronto"
+
+strReplace = str.lower().replace("to", "**", 3)
+strReplace1 = str.lower().replace("to", "**")
+
+print strReplace
+print strReplace1
