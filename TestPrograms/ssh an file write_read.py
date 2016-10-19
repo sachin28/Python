bb4112c7aebdd44eab2a7db4ea1f4b97f959c27e
Sachin Shukla <sachin28@users.noreply.github.com>
2016-10-19 13:05:28 -0700

    dynamic for input

--- TestPrograms/ssh an file write_read.py
+++ TestPrograms/ssh an file write_read.py
@@ -1,23 +1,34 @@
 import paramiko
 
+l_host = raw_input("Enter host: ")
+l_user = raw_input("Enter user: ")
+l_password = raw_input('Enter the password of ' +str(l_host) +' user ' +str(l_user) +': ')
 
-l_password = "#password"
-l_host = "#ip"
-l_user = "#user eg root"
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(l_host, username=l_user, password=l_password)
-transport = ssh.get_transport()
-session = transport.open_session()
-session.set_combine_stderr(True)
-session.get_pty()
-#for testing purposes we want to force sudo to always to ask for password. because of that we use "-k" key
-session.exec_command("ls -l")
-stdin = session.makefile('wb', -1)
-stdout = session.makefile('rb', -1)
-#you have to check if you really need to send password here
-stdin.write(l_password +'\n')
-stdin.flush()
-for line in stdout.read().splitlines():
-    print 'host: %s: %s' % (l_host, line)
-    
+def connection():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(l_host, username=l_user, password=l_password)
+    return ssh
+
+def command(ssh):
+    transport = ssh.get_transport()
+    session = transport.open_session()
+    session.set_combine_stderr(True)
+    session.get_pty()
+    session.exec_command("ls -l")
+    stdin = session.makefile('wb', -1)
+    stdout = session.makefile('rb', -1)
+
+    with open('SampleWrite.txt', 'w') as w:
+        w.write(stdout.read())
+    # print (stdout.read())    why cant we call it again
+    stdin.flush()
+
+#write contents of the command from server to local machine and close the connection
+if __name__ == '__main__':
+    ssh = connection()
+    c = command(ssh)
+    ssh.close()
+
+
+
