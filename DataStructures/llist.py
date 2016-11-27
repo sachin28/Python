cc050a729bd48b084ed696ee3eed4d1f7e42dde1
sachin28 <shuklasachin.ec@gmail.com>
2016-11-26 19:49:21 -0800

    dynamic insertion

--- DataStructures/llist.py
+++ DataStructures/llist.py
@@ -1,31 +1,51 @@
-##Ref: Geeks for Geeks
+## Ref: Geeks for Geeks
 
 class Node:
+    def __init__(self, data):
+        self.next = None
+        self.data = data
 
-  def __init__(self, data):
-    self.next = None
-    self.data = data
 
 class LinkedList:
 
-  def __init__(self):
-    self.head = None
+    def __init__(self):
+        self.head = None
 
-  def printlist(self):
-    temp = self.head
-    while(temp):
-      print temp.data
-      temp = temp.next
+    def inset(self, value):
+        if self.head is None:
+            new_node = Node(value)
+            self.head = new_node
+
+        else:
+            new_node = Node(value)
+            temp_node = self.head
+            while (temp_node.next != None):
+                temp_node = temp_node.next
+
+            temp_node.next = new_node
+
+    def printlist(self):
+        temp = self.head
+        while(temp):
+            print temp.data
+            temp = temp.next
+
+
+
+
+
 
 if __name__ == '__main__':
 
-  llist = LinkedList()
+    llist = LinkedList()
+    for num in range(0, 5):
+        llist.inset(value=num)
 
-  llist.head = Node(1)
-  second = Node(2)
-  third = Node(3)
 
-  llist.head.next = second
-  second.next = third
 
-  llist.printlist()
+    llist.printlist()
+
+
+
+
+
