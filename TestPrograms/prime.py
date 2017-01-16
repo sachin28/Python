11dba2578864ee6bbc8a2a0d8a6b9f55f627f63b
sachin28 <shuklasachin.ec@gmail.com>
2017-01-15 21:09:16 -0800

    Handled negative numbers,0 and 1

--- TestPrograms/prime.py
+++ TestPrograms/prime.py
@@ -2,17 +2,21 @@ def prime():
     'checking prime for 9 numbers'
     for i in range (10):
         if i:
-            number = int(raw_input("\r\nInsert any number to check its primarility: "))
-            flag = 0
-            for x in range(2,(number/2)+1):
-                if number % x == 0:
-                    flag = 1
-                    break
+            number = abs(int(raw_input("\r\nInsert any number to check its primarility: ")))
 
-            if flag == 1:
-                print +number," not a prime"
+            if number == 0 or number == 1:
+                print "not a prime"
+
             else:
-                print +number, "is a prime"
+                flag = 0
+                for x in range(2,(number/2)+1):
+                    if number % x == 0:
+                        flag = 1
+                        break
 
+                if flag == 1:
+                    print "not a prime"
+                else:
+                    print "is a prime"
 
 prime()
