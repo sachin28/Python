60d3c1c4dabb990b04b7329e0e6bd8ddcd2cc853
sachin28 <shuklasachin.ec@gmail.com>
2017-01-26 15:38:35 -0800

    Handled for negative numbers

--- MustKnowPrograms/sortList_maxtomin.py
+++ MustKnowPrograms/sortList_maxtomin.py
@@ -1,3 +1,4 @@
+#k = int(raw_input("enter the k value: ")) #for kth largest value
 unsorted_list = [2,3,5,-1,8,2,99]
 sorted_list = []
 
@@ -12,6 +13,8 @@ while unsorted_list:
     sorted_list.append(max)
     unsorted_list.remove(max)
 
-#print unsorted_list
 print sorted_list
+
+
+#print sorted_list[k-1]   #for kth largest value
 
