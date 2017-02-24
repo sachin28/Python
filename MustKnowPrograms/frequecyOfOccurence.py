f6a18fb0e1a46b6fc4daf44e10265e3c05eed60e
Sachin Shukla <sachin28@users.noreply.github.com>
2017-02-24 11:58:12 -0800

    Update frequecyOfOccurence.py

--- MustKnowPrograms/frequecyOfOccurence.py
+++ MustKnowPrograms/frequecyOfOccurence.py
@@ -1,3 +1,38 @@
+def frequency(filename):
+    wordcount = {}
+    file = open(filename, 'r')
+    maxword = None
+    maxcount = 0
+
+    for line in file:
+
+        word_list = line.replace(',', '').replace('.', '').lower().split()
+
+        for word in word_list:
+
+            if word in wordcount:
+                wordcount[word] += 1
+            else:
+                wordcount[word] = 1
+
+            if wordcount[word] > maxcount:
+                maxcount = wordcount[word]
+                maxword = word
+
+    print '{:20}{:4}'.format('word', 'frequency')
+    print '-' * 30
+    for word, frequency in wordcount.iteritems():
+        print '{:20}{:4}'.format(word, frequency)
+    print 'maximum word is "{}" with "{}" repetitions'.format(maxword, maxcount)
+    
+    file.close()
+
+
+if __name__ == "__main__":
+    frequency('test.txt')
+
+
+'''
 file = open('names.txt')
 line = file.readline()
 count_list = {}
@@ -24,3 +59,4 @@ print max_word
 print max_count
 
 print max(count_list.values())
+'''
