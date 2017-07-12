026ce141b3f437f9d6ad79f45ba5a7337e828ddd
sachin28 <shuklasachin.ec@gmail.com>
2017-07-12 15:36:16 -0700

    flask learning

--- MustKnowPrograms/test1.py
+++ MustKnowPrograms/test1.py
@@ -1,1 +1,27 @@
-print "Sachin"
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/admin')
+
+def admin_name():
+    return "Hello Admin"
+
+@app.route('/guest/<guest>')
+def guest_name(guest):
+    return "Hello %s" %guest
+
+@app.route('/user/<user>')
+def user_name(user):
+    if user == 'admin':
+        return (redirect(url_for('admin_name')))
+
+    else:
+        return (redirect(url_for('guest_name',guest=user)))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#credits: www.tutorialspoint.com
