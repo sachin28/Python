26dd7a276132d01fb8e7f38360ba826c61c2f5af
Sachin Shukla <sachin28@users.noreply.github.com>
2021-02-08 15:58:59 -0800

    Update Fibonacci.py

--- MustKnowPrograms/Fibonacci.py
+++ MustKnowPrograms/Fibonacci.py
@@ -2,7 +2,7 @@ val = int(raw_input("Enter the number to get fib: "))
 
 
 def fibo(val):
-    if val == 1 or val == 2:
+    if val == 0 or val == 1:
         return val
 
     return fibo(val - 1) + fibo(val - 2)
